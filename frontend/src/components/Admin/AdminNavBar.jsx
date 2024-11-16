90aef82fc917ae18baa3eeb2707b669a2cff332a
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-16 18:22:37 +0530

    Add new User button Added

--- frontend/src/components/Admin/AdminNavBar.jsx
+++ frontend/src/components/Admin/AdminNavBar.jsx
@@ -17,6 +17,11 @@ const AdminNavBar = () => {
                 </div>
                 <div className="flex space-x-4">
                     <button
+                        className="bg-violet-500 text-white px-4 py-2 rounded hover:bg-violet-600"
+                    >
+                        Add New User
+                    </button>
+                    <button
                         onClick={handleLogout}
                         className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
                     >
