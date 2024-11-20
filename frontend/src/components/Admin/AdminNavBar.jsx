123bd4e4b271b198f2af2859c618295d9bb40519
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-20 21:07:23 +0530

    make this more responsiveness

--- frontend/src/components/Admin/AdminNavBar.jsx
+++ frontend/src/components/Admin/AdminNavBar.jsx
@@ -24,37 +24,37 @@ const AdminNavBar = () => {
     return (
         <nav className="bg-gray-800 p-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-semibold">
+                <div className="text-white text-lg font-semibold hidden lg:block">
                     <Link to="/dashboard" className="hover:text-gray-300">Admin Dashboard</Link>
                 </div>
-                <div className="text-white text-lg font-semibold">
+                <div className="text-white text-lg font-semibold flex justify-center lg:justify-start">
                     <input
                         type="text"
                         value={searchQuery}
                         onChange={handleSearch}
                         placeholder="Search users..."
-                        className="bg-gray-700 text-white px-3 py-1 rounded focus:outline-none focus:ring focus:ring-violet-500"
+                        className="w-52 max-w-xs sm:max-w-sm lg:max-w-md bg-gray-700 text-white px-3 py-1 rounded focus:outline-none focus:ring focus:ring-violet-500"
                     />
                 </div>
-                <div className="flex space-x-4">
+
+                <div className="flex flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
                     <button
                         onClick={() => toggleModal(!isModalOpen)}
-                        className="bg-violet-500 text-white px-4 py-2 rounded hover:bg-violet-600"
+                        className="bg-violet-500 text-white px-4 py-2 rounded hover:bg-violet-600 w-full lg:w-auto"
                     >
                         Add New User
                     </button>
                     <button
                         onClick={handleLogout}
-                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 w-full lg:w-auto"
                     >
                         Logout
                     </button>
-                    {
-                        isModalOpen && (
-                            <AddUser isModalOpen={isModalOpen} toggleModal={() => toggleModal(false)} />
-                        )
-                    }
+                    {isModalOpen && (
+                        <AddUser isModalOpen={isModalOpen} toggleModal={() => toggleModal(false)} />
+                    )}
                 </div>
+
             </div>
         </nav>
     )
